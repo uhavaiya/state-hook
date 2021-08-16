c336bc8a41cc32244f42ffa97bd53f843bc94401
Umesh <uhavaiya@gmail.com>
2021-08-16 11:40:00 +0530

    useEffect

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function HookCounter() {
-
     const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`
+    })
 
     return (
         <div>
-            <button onClick={()=> setCount(count+1) }>Count {count } </button>
+            <button onClick={() => setCount(count + 1)}>Count {count} </button>
         </div>
     )
 }
